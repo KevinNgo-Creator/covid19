ee858a4dd3f0c9ae210cb45e20000ccf9a861ca0
Code Master <jeffbell.kc@gmail.com>
2020-05-06 21:28:59 -0500

    merge code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import DashBoard from "./dashboard/components/App.js";
 import NoMatch from "./pages/NoMatch";
 import Header from "./components/Header";
 import Login from "./pages/Login";
@@ -15,7 +15,7 @@ function App() {
             <Login />
           </Route>
           <Route exact path="/dashboard">
-            <Dashboard />
+            <DashBoard />
           </Route>
           <Route>
             <NoMatch />
